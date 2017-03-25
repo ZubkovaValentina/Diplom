51b45401bfe994bbf803a4a386e889d49d000868
Tepex <tepex@mail.ru>
2017-03-26 02:44:17 +0300

    DB connection

---
+++ mvp/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+class Config
+{
+	const DB_HOST = 'diplom.local';
+	const DB_USER = 'root';
+	const DB_PWD = 'kp580bm80a';
+	const DB_NAME = 'remont_avto';
+}
--- mvp/models/Model.php
+++ mvp/models/Model.php
@@ -1,7 +1,23 @@
 <?php
 
+require_once 'Config.php';
+
 abstract class Model
 {
+	protected $db;
+	
+	function __construct()
+	{
+		error_reporting(E_ALL & ~E_DEPRECATED);
+		$this->db = mysql_connect(Config::DB_HOST, Config::DB_USER, Config::DB_PWD);
+		mysql_select_db(Config::DB_NAME, $this->db);
+	}
+	
+	function __destruct()
+	{
+		mysql_close($this->db);
+	}
+	
 	abstract function getTitle();
 	abstract function getHeader();
 	abstract function getData();
